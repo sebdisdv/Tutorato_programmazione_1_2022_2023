0167cfd49e5f71195b73396a3b5e3811875ce344
Thomas <thomasdemin16@gmail.com>
2022-10-17 17:18:37 +0200

    Simplify mcd.

--- 2022_10_17/es1.cc
+++ 2022_10_17/es1.cc
@@ -12,17 +12,9 @@ int mcd_rec(int n1, int n2){
     return retval;
 }
 
-int mcd(int n1, int n2){
-    if (n1 > n2){
-        return mcd_rec(n1, n2);
-    } else {
-        return mcd_rec(n2, n1);
-    }
-}
-
 int main(){
     int n1, n2;
     cout << "Inserire due numeri interi positivi: ";
     cin >> n1 >> n2;
-    cout << "MDC= " << mcd(n1, n2) << endl;
+    cout << "MDC= " << mcd_rec(n1, n2) << endl;
 }
