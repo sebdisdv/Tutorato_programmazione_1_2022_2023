dc076309686210bbe3eb176217d7242fc6c1ed38
Thomas <thomasdemin16@gmail.com>
2022-12-05 15:57:30 +0100

    Add 3es0.cc

---
+++ 2022_12_05/es0.cc
@@ -0,0 +1,81 @@
+#include<iostream>
+
+using namespace std;
+
+struct stack{
+    int* buffer;
+    int head;
+    int curr_size;
+    int max_size;
+};
+
+void init(stack &s, int dim);
+bool push(stack &s, int val);
+bool first(stack &s, int& ret);
+bool pop(stack &s);
+void dealloc(stack &s);
+
+int main() {
+    stack s;
+    init(s, 3);
+    int ret;
+
+    cout << push(s, 1) << endl;
+
+    cout << push(s, 2) << endl;
+
+    int f;
+    cout << first(s, f) << " " << f << endl;
+    cout << pop(s) << endl;
+    cout << first(s, f) << " " << f << endl;
+
+    cout << push(s, 3) << endl;
+    cout << first(s, f) << " " << f << endl;
+    
+    dealloc(s);
+    return 0;
+}
+
+void init(stack &s, int dim) {
+    s.buffer = new int[dim];
+    s.head = 0;
+    s.curr_size = 0;
+    s.max_size = dim;
+}
+
+bool push(stack &s, int val) {
+    bool result = true;
+    if (s.curr_size < s.max_size){
+        s.buffer[s.head] = val;
+        s.head = (s.head + 1) % s.max_size;
+        s.curr_size++;
+    } else {
+        result = false;
+    }
+    return result;
+}
+
+bool first(stack &s, int& ret) {
+    bool result = true;
+    if (s.curr_size > 0){
+        ret = s.buffer[(s.head - 1 + s.max_size) % s.max_size];
+    } else {
+        result = false;
+    }
+    return result;
+}
+
+bool pop(stack &s) {
+    bool result = true;
+    if (s.curr_size > 0) {
+        s.head = (s.head - 1 + s.max_size) % s.max_size;
+        s.curr_size--;
+    } else {
+        result = false;
+    }
+    return result;
+}
+
+void dealloc(stack &s){
+    delete s.buffer;
+}
